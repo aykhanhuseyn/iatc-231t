2c7edaa3e0b29822dbc1972b6e064d7c12736412
aykhanhuseyn <immense_ideas@icloud.com>
2023-06-02 20:46:49 +0400

    feat: add task 2

--- JavaScript/lesson-19/task.js
+++ JavaScript/lesson-19/task.js
@@ -6,3 +6,9 @@ console.log(String.fromCharCode(53)); // 5
 // output: hello world
 // example: [83, 97, 108, 97, 109, 32, 65, 122, 101, 114, 98, 97, 121, 99, 97, 110]
 // example: [106, 97, 118, 97, 115, 99, 114, 105, 112, 116, 32, 105, 115, 32, 98, 111, 109, 98, 97]
+
+// 2. Write a JavaScript program to convert a string to an array of ASCII codes.
+// example: hello world
+// output: [104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]
+// example: Salam Azerbaycan
+// example: javascript is bomba
