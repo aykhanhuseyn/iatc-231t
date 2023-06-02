58380e1f1c5d52b57f432e53da5943ddb88da802
aykhanhuseyn <immense_ideas@icloud.com>
2023-06-02 20:44:13 +0400

    feat: add task 1

---
+++ JavaScript/lesson-19/task.js
@@ -0,0 +1,5 @@
+console.log(String.fromCharCode(65)); // A
+console.log(String.fromCharCode(53)); // 5
+
+// 1. Write a JavaScript program to display numbers as worlds using ASCII code.
+// example: [104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]
