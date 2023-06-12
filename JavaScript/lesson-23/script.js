a45c88e4eadfc269d04225361d4b5c45bcf54de3
aykhanhuseyn <immense_ideas@icloud.com>
2023-06-12 21:57:20 +0400

    feat: add math and date

---
+++ JavaScript/lesson-23/script.js
@@ -0,0 +1,49 @@
+// let date = new Date();
+
+// console.log(typeof Date); // function
+
+// console.log(date.getDate(), date.getMonth(), date.getFullYear());
+
+// console.log('DD.MM.YYYY');
+// console.log(
+// 	`${String(date.getDate()).padStart(2, '0')}.${String(
+// 		date.getMonth() + 1,
+// 	).padStart(2, '0')}.${date.getFullYear()}`,
+// );
+
+// date.getTime(); // 1616428800000 => timestamp
+// // 1 jan 1970 00:00:00
+
+// new Date(1616428800000); // Fri Mar 19 2021 02:00:00 GMT+0400 (Azerbaijan Standard Time)
+
+// new Date(2019, 0, 1, 0, 0, 0, 0); // Tue Jan 01 2019 00:00:00 GMT+0400 (Azerbaijan Standard Time)
+
+console.log(typeof Math); // object
+
+// console.log(Math.PI); // 3.141592653589793
+
+// console.log('Math.round');
+// console.log(3.5, Math.round(3.5)); // 4
+// console.log(3.4, Math.round(3.4)); // 3
+
+// console.log('Math.ceil');
+// console.log(3.1, Math.ceil(3.1)); // 4
+// console.log(3.9, Math.ceil(3.9)); // 4
+
+// console.log('Math.floor');
+// console.log(3.9, Math.floor(3.9)); // 3
+// console.log(3.1, Math.floor(3.1)); // 3
+
+console.log('Math.random');
+console.log(Math.random()); // 0.123456789
+
+console.log('Math.random * 10');
+
+console.log(Math.pow(2, 3)); // 8
+
+console.log(Math.sqrt(9)); // 3
+
+console.log(Math.abs(-10)); // 10
+
+console.log(Math.max(1, 2, 3, 4, 5)); // 5
+console.log(Math.min(1, 2, 3, 4, 5)); // 1
